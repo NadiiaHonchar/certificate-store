57f560df169bed6452f1746b84917ed92a990eeb
NadiiaHonchar <17nmg82@gmail.com>
2022-08-30 23:57:46 +0300

    added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">        
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">        
+    //   </header>
+    // </div>
+    <Header></Header>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import style from "./Header.module.css";
+const Header = () => {
+  return (
+    <>
+      <header className={style.header}>
+        <div className={style.containerHeader}>
+          
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
