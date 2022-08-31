cdb2a38217bc138e6ca8de7cb924c8494db5cfe6
NadiiaHonchar <17nmg82@gmail.com>
2022-09-01 00:06:51 +0300

    add drag&droop

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
-import './App.css';
-import Header from './components/Header';
+import { useState } from "react";
+import "./App.css";
+// const ASN1 = require('@lapo/asn1js');
+import PEM from "asn1-parser";
+// const Hex = require('@lapo/asn1js/hex');
+// import jseu from 'js-encoding-utils';
+// import DropArea from './components/DropeArea';
+// import Header from './components/Header';
+// import * as certInfo from 'cert-infos';
+
 
 function App() {
+  const [drag, setDrag] = useState(false);
+
+  function dragStartHandler(e){
+    e.preventDefault();
+    setDrag(true);
+  }
+
+  function dragLeaveHandler(e){
+    e.preventDefault();
+    setDrag(false);
+  }
+
+  function onDropeHandler(e){
+    e.preventDefault();
+    let files = [...e.dataTransfer.files];
+//     const result = ASN1.decode(files);
+// if (result.typeName() !== 'SEQUENCE') {
+//     throw 'Неправильна структура конверта сертифіката (очікується SEQUENCE)';
+// }
+// const tbsCertificate = result.sub[0];
+
+// const {certificate} = certInfo.getRSACertInfoPem(files);
+// console.log('The short certificate information:');
+// console.log(certificate);
+
+// const decoded = jseu.encoder.decodeBase64(files);
+// const decoded = jseu.encoder.decodeBase64(files);
+// const binCert = jseu.encoder.hexStringToArrayBuffer(files);
+var der = PEM.parseBlock(files).der;
+// var json = ASN1.parse(files);
+
+
+// const result = ASN1.decode(files);
+
+
+// require([
+//   'https://unpkg.com/@lapo/asn1js/asn1.js',
+//   'https://unpkg.com/@lapo/asn1js/hex.js'
+// ], function(ASN1, Hex) {
+//   document.body.innerText = ASN1.decode(Hex.decode('06032B6570')).content();
+// });
+
+    console.log(der)
+    setDrag(false)
+  }
+
   return (
-    // <div className="App">
-    //   <header className="App-header">        
-    //   </header>
-    // </div>
-    <Header></Header>
+    <div className="app">
+      {drag ? (
+        <div className="drop-area" 
+        onDragStart={e=>dragStartHandler(e)} 
+        onDragLeave={e=>dragLeaveHandler(e)} 
+        onDragOver={e=>dragStartHandler(e)}
+        onDrop={e=>onDropeHandler(e)}> Відпустіть файл для завантажування</div>
+      ) : (
+        <div onDragStart={e=>dragStartHandler(e)} 
+        onDragLeave={e=>dragLeaveHandler(e)} 
+        onDragOver={e=>dragStartHandler(e)}> Перетягніть файл сертифіката у поле</div>
+      )}
+    </div>
   );
 }
 
---
+++ src/components/GetRequest.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PropTypes from "prop-types";
+
+const API_KEY = "69b52966b17106982b5faaf8fa655b3b";
+const GetRequest = (query, get, typeQuery) => {
+  const [dateResponse, setDateResponse] = useState(() => []);
+
+  useEffect(() => {
+    if (!query || !get) {
+      return;
+    }
+    async function getUser() {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/${query}?api_key=${API_KEY}&${get}`
+        );
+        if (typeQuery === "general") {
+          setDateResponse(response.data.results);
+        }
+        if (typeQuery === "details") {
+          setDateResponse(response.data);
+        }
+        
+      } catch (error) {
+        return toast(
+          `Sorry, an error ${error} has occurred. Please try again.`
+        );
+      }
+    }
+    getUser();
+  }, [query, get, typeQuery]);
+
+  return dateResponse;
+};
+
+export default GetRequest;
+
+GetRequest.propTypes = {
+  query: PropTypes.string.isRequired,
+  get: PropTypes.string.isRequired,
+  typeQuery: PropTypes.string.isRequired,
+};
